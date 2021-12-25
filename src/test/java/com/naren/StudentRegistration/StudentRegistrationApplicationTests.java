7ba69b45b7309a219251025c33cba1383b495fc8
nksharma514 <nksharma514@gmail.com>
2021-12-26 00:13:09 +0530

    init spring boot

---
+++ src/test/java/com/naren/StudentRegistration/StudentRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naren.StudentRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
